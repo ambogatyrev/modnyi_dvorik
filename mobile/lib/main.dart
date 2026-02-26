620a1501f9ab49f65ed8a3672bff4aa404847c26
Ahmet Bogatyrev <sarmak9044@gmail.com>
2026-02-26 14:34:22 +0300

    created web MVP version

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -60,6 +60,27 @@ class ModnyiDvorikApp extends StatelessWidget {
         theme: AppTheme.lightTheme,
         debugShowCheckedModeBanner: false,
         routerConfig: AppRouter.router,
+        builder: (context, child) {
+          return ColoredBox(
+            color: const Color(0xFF1A1A2E),
+            child: Center(
+              child: Container(
+                constraints: const BoxConstraints(maxWidth: 430),
+                decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withValues(alpha: 0.3),
+                      blurRadius: 24,
+                      spreadRadius: 4,
+                    ),
+                  ],
+                ),
+                clipBehavior: Clip.hardEdge,
+                child: child,
+              ),
+            ),
+          );
+        },
       ),
     );
   }
